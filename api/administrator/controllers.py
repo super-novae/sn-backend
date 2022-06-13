10913f81cd3c321823624eae8735d9fa21619b89
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-06-13 23:37:30 +0000

    Add some changes to the administrator api urls

--- api/administrator/controllers.py
+++ api/administrator/controllers.py
@@ -5,7 +5,10 @@ from .errors import AdministratorNotFound
 
 # Initiate module blueprint
 administrator = APIBlueprint(
-    "administrator", __name__, tag="Administrator", url_prefix="/api/v1/administrators"
+    "administrator",
+    __name__,
+    tag="Administrator",
+    url_prefix="/api/v1/elections/<election_id>/administrators",
 )
 
 
@@ -16,7 +19,7 @@ administrator = APIBlueprint(
     summary="Administrator Sign Up",
     description="An endpoint for the creation of administrators",
 )
-def administrator_sign_up(data):
+def administrator_sign_up(election_id, data):
     pass
 
 
@@ -27,7 +30,7 @@ def administrator_sign_up(data):
     summary="Administrator Login",
     description="An endpoint for the login of administrators",
 )
-def administrator_login(data):
+def administrator_login(election_id, data):
     pass
 
 
@@ -37,15 +40,15 @@ def administrator_login(data):
     summary="Administrator Get All",
     description="An endpoint to get all administrators",
 )
-def administrator_get_all():
+def administrator_get_all(election_id):
     pass
 
 
-@administrator.get("/<id>")
+@administrator.get("/<admin_id>")
 @administrator.output(AdministratorSchema)
 @administrator.doc(
     summary="Administrator Get By Id",
     description="An endpoint to an administrator by ID",
 )
-def administrator_get_by_id(id):
+def administrator_get_by_id(election_id, id):
     raise AdministratorNotFound
