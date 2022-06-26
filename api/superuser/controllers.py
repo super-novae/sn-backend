fff0441a201ca447f1e48a6e208da394ebc87959
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-06-26 17:13:01 +0000

    feat: Add superuser module

---
+++ api/superuser/controllers.py
@@ -0,0 +1,29 @@
+from apiflask import APIBlueprint
+from flask_jwt_extended import create_access_token
+from .errors import *
+from .models import Superuser
+from .schema import *
+
+# Initiate Super User module blueprint
+superuser = APIBlueprint(
+    "superuser", __name__, tag="Superuser", url_prefix="/api/v1/superusers"
+)
+
+
+@superuser.post("/login")
+@superuser.input(SuperuserLoginSchema)
+@superuser.output(SuperUserSchema)
+@superuser.doc(
+    summary="Superuser Login", description="An endpoint for login of the superuser"
+)
+def superuser_login(data):
+    superuser: Superuser = Superuser.find_by_username(username=data["username"])
+
+    if superuser:
+        superuser_password_is_correct = superuser.verify_superuser(
+            password=data["password"]
+        )
+        if superuser_password_is_correct:
+            superuser.auth_token = create_access_token(superuser.public_id)
+            return superuser
+    raise SuperuserWithCredentialsDoesNotExist
---
+++ api/superuser/models.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from api.extensions import db, bcrypt
+from os import environ
+from dotenv import load_dotenv
+import secrets
+
+# Load environment variables
+load_dotenv()
+
+
+class Superuser(db.Model):
+    __tablename__ = "sn_superuser"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    public_id = db.Column(db.String(length=130), nullable=False, unique=True)
+    name = db.Column(db.String(length=80), nullable=False)
+    username = db.Column(db.String(length=15), nullable=False, unique=True)
+    email = db.Column(db.String(length=80), nullable=False, unique=True)
+    date_created = db.Column(db.DateTime(), nullable=False)
+    password_hash = db.Column(db.String(length=130), nullable=False)
+
+    def __init__(self, name, username, email):
+        self.public_id = secrets.token_hex(64)
+        self.name = name
+        self.username = username
+        self.email = email
+        self.date_created = datetime.today()
+
+    @property
+    def password():
+        return "Password can only be set"
+
+    @property().setter
+    def password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(
+            password, environ.get("BCRYPT_LOG_ROUNDS")
+        ).decode(encoding="utf-8", errors="ignore")
+
+    def verify_superuser(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+    @classmethod
+    def find_by_public_id(cls, public_id):
+        return cls.query.filter_by(public_id=public_id).first()
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
