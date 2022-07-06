ada3f3d561578e0962f893234a41fd3edd16f5c6
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-07-06 13:46:55 +0000

    feat: Add black tests file for election module

---
+++ api/tests/test_election.py
@@ -0,0 +1,209 @@
+from .setup import truncate_db_tables
+
+
+def test_election_create_successful():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_create_unauthorized():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_modify_successful():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_modify_unauthorized():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_modify_non_existent():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_delete_successful():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_delete_unauthorized():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_delete_non_existent():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_get_by_id_successful():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_get_by_id_unauthorized():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_get_by_id_non_existent():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_get_all_by_organization_id_successful():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_get_all_by_organization_id_unauthorized():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_create_candidate_successful():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_create_candidate_unauthorized():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_modify_candidate_successful():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_modify_candidate_unauthorized():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_modify_candidate_non_existent():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_delete_candidate_successful():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_delete_candidate_unauthorized():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_delete_candidate_non_existent():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_get_candidate_by_id_successful():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_get_candidate_by_id_unauthorize():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_get_candidate_by_id_non_existent():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_get_all_candidates_by_election_id_successful():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
+
+
+def test_election_get_all_candidates_by_election_id_unauthorized():
+    # Clear database before tests
+    truncate_db_tables()
+
+    # Clear database after tests
+    truncate_db_tables()
