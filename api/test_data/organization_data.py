68ee40c27fc87069c2829e1a4e6614180f14d4b5
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-07-18 12:30:01 +0000

    fix: Add new info to testing data

--- api/test_data/organization_data.py
+++ api/test_data/organization_data.py
@@ -1,18 +1,20 @@
 from api.extensions import db, fake
 from ..organization.models import Organization
+from ..test_data.admin_data import administrator_get_test_instance
 
-data = {"name": fake.company()}
 
 
 def organization_details():
-    return data
+    admin = administrator_get_test_instance()
+    return {"name": fake.company(), "administrator_id": admin.id}
 
 
 def organization_modified_details():
     return {"name": fake.company()}
 
 
-def organization_create():
+def organization_create() -> Organization:
+    data = organization_details()
     organization = Organization(**data)
 
     db.session.add(organization)
