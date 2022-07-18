01a26705629194ca45b900516474d96a2635d067
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-07-18 12:31:23 +0000

    fix: Apply minor fixes to the organization module

--- api/organization/models.py
+++ api/organization/models.py
@@ -9,10 +9,12 @@ class Organization(db.Model):
     public_id = db.Column(db.String(length=32), nullable=False, unique=True)
     name = db.Column(db.String(length=256), nullable=False)
     elections = db.relationship("Election")
+    administrator_id = db.Column(db.Integer, db.ForeignKey("sn_administrator.id"), nullable=False)
 
-    def __init__(self, name):
+    def __init__(self, name, administrator_id):
         self.public_id = f"org-{token_hex()[:28]}"
         self.name = name
+        self.administrator_id = administrator_id
 
     @classmethod
     def find_by_public_id(cls, public_id):
--- api/organization/schema.py
+++ api/organization/schema.py
@@ -1,8 +1,12 @@
 from apiflask import Schema
-from apiflask.fields import String
+from apiflask.fields import String, Integer
 from apiflask.validators import Length
 
 
 class OrganizationSchema(Schema):
+    administrator_id = Integer(required=True)
+    name = String(required=True, validate=[Length(4, 256)])
     public_id = String(required=True, dump_only=True, validate=[Length(32)])
+
+class OrganizationModifySchema(Schema):
     name = String(required=True, validate=[Length(4, 256)])
