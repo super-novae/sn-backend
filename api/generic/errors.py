80f276ff95954daa38bf137bf94f7edf848fcb55
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-07-30 08:44:31 +0000

    fix: Moved data to separate generic module

--- api/generic/errors.py
+++ api/generic/errors.py
@@ -4,3 +4,7 @@ from apiflask import HTTPError
 class UserDoesNotHaveRequiredRoles(HTTPError):
     message: str = "User does not have the required permissions to perform action"
     status_code: int = 403
+
+class InternalServerError(HTTPError):
+    message: str = "Request cannot be processed at the moment"
+    status_code: int = 500
