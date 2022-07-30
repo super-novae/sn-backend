12a1f48f2919a5e1389d186d6b144ecd10c3cfdd
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-07-30 08:45:11 +0000

    update: Add logging config

--- api/extensions.py
+++ api/extensions.py
@@ -1,10 +1,11 @@
+from flask import current_app
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from faker import Faker
-from blinker import Namespace
+from werkzeug.local import LocalProxy
 
 cors = CORS()
 db = SQLAlchemy()
@@ -12,4 +13,4 @@ migrate = Migrate()
 bcrypt = Bcrypt()
 jwt = JWTManager()
 fake = Faker()
-api_signals = Namespace()
+logger = LocalProxy(lambda: current_app.logger)
