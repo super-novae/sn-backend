0bcb20294f45e7a65ee291e3aa677ddfc84a80a1
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-08-30 15:43:47 +0000

    update: previous message

--- api/superuser/schema.py
+++ api/superuser/schema.py
@@ -1,6 +1,6 @@
 from apiflask import Schema
 from apiflask.fields import String
-from apiflask.validators import Length, Regexp
+from apiflask.validators import Length
 
 
 class SuperUserSchema(Schema):
@@ -14,16 +14,4 @@ class SuperuserLoginSchema(Schema):
         validate=[
             Length(min=8, max=16),
         ],
-        metadata={
-            "description": """
-            * Password should contain at least one lowercase letter\n
-            * Password should contain at least one uppercase letter\n
-            * Password should contain at least one special character [!@#$%^&*()_+-=]\n
-            * Password should have a minimum length of 8 and a maximum length of 16
-            """
-        },
     )
-
-# Regexp(
-#     "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+-=])[A-Za-z\d!@#$%^&*()_+-=]{8,}$"
-# ),
