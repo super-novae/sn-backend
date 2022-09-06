738ae5ce61a5e86d25b9b87a0c7482150bb3e95f
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-09-06 12:18:03 +0000

    update: Add new function to send passwords to admin after they're created

--- api/generic/mail.py
+++ api/generic/mail.py
@@ -15,3 +15,18 @@ def send_password_to_voter(voter_email: str, voter_name: str, password: str):
     """
     mail.send(msg)
     return "Message sent"
+
+def send_password_to_admin(admin_email: str, admin_name: str, password: str):
+    msg = Message(
+        "KNUST Voting Details",
+        sender="thesupernovaegroups@gmail.com",
+        recipients=[admin_email],
+    )
+    msg.body = f"""
+    Hello {admin_name},
+    You have been granted administrator privileges
+    Find below your password for the KNUST Voting Platform
+    Password: {password}
+    """
+    mail.send(msg)
+    return "Message sent"
