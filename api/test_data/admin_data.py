47393909d9c869f446a4dd63d652652dd534fe0e
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-09-08 15:15:05 +0000

    update: Add seed to administrator generation function

--- api/test_data/admin_data.py
+++ api/test_data/admin_data.py
@@ -1,27 +1,33 @@
-from api.extensions import fake
+from api.extensions import fake, db
 from api.administrator.models import Administrator
 from .superuser_data import superuser_login
+from ..generic.password import generate_password
+import random
 
 data = {
     "email": fake.email(),
     "name": fake.name(),
-    "password": fake.password(length=15),
     "username": fake.user_name()[:8],
 }
+# "password": fake.password(length=15),
 
 
-def administrator_signup(client):
+def administrator_signup(client, seed=None):
+    # Seed random function
+    if seed: 
+        random.seed(seed)
+
     logged_in_superuser = superuser_login(client)
 
-    response = client.post(
-        "/api/v1/administrators/signup",
-        json=administrator_signup_correct_credentials(),
-        headers={
-            "Authorization": f"Bearer {logged_in_superuser['auth_token']}"
-        },
-    )
+    admin_details = administrator_signup_correct_credentials()
 
-    return response.json
+    admin = Administrator(**admin_details)
+    admin.password = generate_password()
+
+    db.session.add(admin)
+    db.session.commit()
+
+    return admin
 
 
 def administrator_signup_correct_credentials():
@@ -32,25 +38,26 @@ def administrator_signup_username_exists():
     return {
         "email": fake.email(),
         "name": fake.name(),
-        "password": fake.password(length=15),
         "username": data["username"],
     }
+# "password": fake.password(length=15),
 
 
 def administrator_signup_email_exists():
     return {
         "email": data["email"],
         "name": fake.name(),
-        "password": fake.password(length=15),
         "username": fake.user_name()[:8],
     }
-
 
 def administrator_modify():
     return {"name": fake.name(), "username": fake.user_name()[:8]}
 
 
-def administrator_login(client):
+def administrator_login(client, seed=None):
+    if seed:
+        random.seed(seed)
+    
     response = client.post(
         "/api/v1/administrators/login",
         json=administrator_login_correct_credentials(),
@@ -59,12 +66,16 @@ def administrator_login(client):
     return response.json
 
 
-def administrator_login_correct_credentials():
-    return {"email": data["email"], "password": data["password"]}
+def administrator_login_correct_credentials(seed=None):
+    if seed:
+        random.seed(seed)
+    return {"email": data["email"], "password": generate_password()}
 
 
-def administrator_login_wrong_credentials():
-    return {"email": data["email"], "password": data["password"] + "goof"}
+def administrator_login_wrong_credentials(seed=None):
+    if seed:
+        random.seed(seed)
+    return {"email": data["email"], "password": generate_password()[::-1]}
 
 
 def administrator_get_test_instance() -> Administrator:
